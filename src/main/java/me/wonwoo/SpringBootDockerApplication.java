c7cf909479b1b72c1ec941f86248acc727ee49a6
Administrator <admin@example.com>
2016-03-07 04:05:40 +0900

    mysql

--- src/main/java/me/wonwoo/SpringBootDockerApplication.java
+++ src/main/java/me/wonwoo/SpringBootDockerApplication.java
@@ -1,21 +1,34 @@
 package me.wonwoo;
 
+import me.wonwoo.account.Account;
+import me.wonwoo.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
 public class SpringBootDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
 
 
-	@RequestMapping("/")
-	public String hello(){
-		return "Hello Spring boot docker!";
-	}
+    @Autowired
+    private AccountRepository repository;
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> Arrays.asList(
+                new Account(1L,"wonwoo","wonwoo@test.com"),
+                new Account(2L,"kevin","kevin@test.com")
+        ).forEach(repository::save);
+    }
 }
